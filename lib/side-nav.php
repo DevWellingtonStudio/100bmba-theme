1921a50b4557837abdf539f44742380c3113ed40
steelbridge-io <chris@steelbridge.io>
2020-09-04 16:29:45 -0700

    Added dynamic nav

--- lib/side-nav.php
+++ lib/side-nav.php
@@ -2,11 +2,40 @@
 
 add_action( 'genesis_before', 'add_side_nav' );
 function add_side_nav() {
-	echo '<div id="bmbaSidenav" class="sidenav">
-  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-  <a href="#">About</a>
-  <a href="#">Services</a>
-  <a href="#">Clients</a>
-  <a href="#">Contact</a>
-</div>';
+	do_action('bmba_sidebar_navigation');
 }
+
+
+function register_sidebar_menu() {
+	register_nav_menu( 'side-bar-menu' ,__( 'Side Bar Nav' ));
+}
+add_action( 'init', 'register_sidebar_menu' );
+
+// Adds Footer Navigation
+add_action( 'bmba_sidebar_navigation', 'add_bmba_sidebar_nav' );
+function add_bmba_sidebar_nav() {
+
+	$menu_locations = get_nav_menu_locations();
+// Sub nav renders is assigned in Menu editor
+	if($menu_locations && $menu_locations['side-bar-menu'] !=0) :
+		?>
+		<div id="bmbaSidenav" class="sidenav">
+		<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+				<?php
+				wp_nav_menu( array(
+					'theme_location'    => 'side-bar-menu',
+					'depth'             => 2,
+					'container'         => 'div',
+					'container_class'   => 'slidein-nav',
+					'container_id'      => 'bmba-sidemenu',
+					'menu'              => 'nav-slider',
+					'menu_class'        => 'nav-slidein',
+					'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+					'walker'            => new WP_Bootstrap_Navwalker(),
+				) );
+				?>
+		</div>
+	<?php endif; ?>
+
+<?php }
+
