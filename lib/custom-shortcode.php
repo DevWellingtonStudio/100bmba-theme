2b16c6d7355cd6ac488af26e0b477457dc869fb9
steelbridge-io <chris@steelbridge.io>
2020-09-04 12:56:15 -0700

    Updated sidebar nav

--- lib/custom-shortcode.php
+++ lib/custom-shortcode.php
@@ -1,6 +1,6 @@
 <?php
 
-add_shortcode('dotiavatar', 'dotiavatar_function');
-function dotiavatar_function() {
+add_shortcode('side-bar-nav', 'bmba_sidebar_nav');
+function bmba_sidebar_nav() {
 	return '<span class="sidebar-icon" onclick="openNav()">&#9776;</span>';
 }
