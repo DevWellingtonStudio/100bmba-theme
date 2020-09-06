14917bef02ecf21eddef88fe2eb8a7c1eac4b6ee
steelbridge-io <chris@steelbridge.io>
2020-09-05 20:58:20 -0700

    Added new template, updated README.md

---
+++ templates/economic-empowerment.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Template Name:Economic Empowerment
+ *
+ * @package      Wellington Studio Theme
+ * @since        1.0
+ * @link         https://github.com/DevWellingtonStudio/wellington-studio-theme.git
+ * @author       Chris Parsons <chris@steelbridge.io>
+ * @license      http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ *
+ */
+
+genesis();
