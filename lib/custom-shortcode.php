3e616d1cf69056e6d9afe42f31facd839d7184b8
steelbridge-io <chris@steelbridge.io>
2020-09-11 18:50:50 -0700

    Added 1st round of member search results

--- lib/custom-shortcode.php
+++ lib/custom-shortcode.php
@@ -4,3 +4,21 @@ add_shortcode('side-bar-nav', 'bmba_sidebar_nav');
 function bmba_sidebar_nav() {
 	return '<span class="sidebar-icon" onclick="openNav()">&#9776;</span>';
 }
+
+add_shortcode('search_field', 'custom_search');
+function custom_search() {
+
+
+		 return '
+
+		<div id="mem-cat-search-cont" class="mem-search-cat">
+			<h2>Search Members</h2>
+			<!-- Start SearchForm For Member Categories -->
+			<form method="get" class="member-category searchform" role="search" action="'. home_url('/') .'"><fieldset id="members-search-field"><input name="s" type="text" class="members-cpt-search s small-12" placeholder="&#xf002;" style="font-family:Arial, FontAwesome"><input type="hidden" name="post_type" value="member_cpt"/><input type="submit" class="btn accent" style="text-transform:uppercase;" align="center" value="Submit" /></fieldset></form>
+			<!-- End SearchForm -->
+		</div> ';
+
+ }
+
+
+
