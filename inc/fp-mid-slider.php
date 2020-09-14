95ed989735c23796135706785e4b9f31831760e7
steelbridge-io <chris@steelbridge.io>
2020-09-13 22:47:17 -0700

    Added two more sliders to the mid slider

--- inc/fp-mid-slider.php
+++ inc/fp-mid-slider.php
@@ -14,6 +14,14 @@ $midslider4title	= get_theme_mod('midslider4title', $default);
 $midsldr4text		= get_theme_mod('midsldr4text', $default);
 
 
+$wst_mid_slider5	= get_theme_mod('wst_mid_slider5', $default);
+$midslider5title	= get_theme_mod('midslider5title', $default);
+$midsldr5text		= get_theme_mod('midsldr5text', $default);
+
+$wst_mid_slider6	= get_theme_mod('wst_mid_slider6', $default);
+$midslider6title	= get_theme_mod('midslider6title', $default);
+$midsldr6text		= get_theme_mod('midsldr6text', $default);
+
 ?>
 <div class="container-fluid">
     <div id="carouselExampleCaptions" class="carousel slide carousel-fade" data-ride="carousel">
@@ -29,7 +37,16 @@ $midsldr4text		= get_theme_mod('midsldr4text', $default);
         <?php endif;
         if($wst_mid_slider4 !== $default) : ?>
         <li data-target="#carouselExampleCaptions" data-slide-to="3"></li>
-        <?php endif; ?>
+        <?php endif;
+        if($wst_mid_slider5 !== $default) : ?>
+		<li data-target="#carouselExampleCaptions" data-slide-to="4"></li>
+		<?php endif;
+		if($wst_mid_slider6 !== $default) : ?>
+		<li data-target="#carouselExampleCaptions" data-slide-to="5"></li>
+		<?php endif; ?>
+
+
+
 	  </ol>
 	  <div class="carousel-inner">
         <?php if($wst_mid_slider1 !== $default) : ?>
@@ -67,7 +84,25 @@ $midsldr4text		= get_theme_mod('midsldr4text', $default);
             <p><?php if($midsldr4text !== $default){echo $midsldr4text;} ?></p>
           </div>
         </div>
-        <?php endif; ?>
+        <?php endif;
+		if($wst_mid_slider5 !== $default) : ?>
+		<div id="mid-slider-fp-4" class="carousel-item align-items-center">
+		  <img src="<?php if($wst_mid_slider5 !== $default){echo $wst_mid_slider5;} ?>" class="d-block w-100" alt="">
+		  <div class="carousel-caption col-md-4">
+			<h3><?php if($midslider5title !== $default){echo $midslider5title;} ?></h3>
+			<p><?php if($midsldr5text !== $default){echo $midsldr5text;} ?></p>
+		  </div>
+		</div>
+		<?php endif;
+		if($wst_mid_slider6 !== $default) : ?>
+		<div id="mid-slider-fp-4" class="carousel-item align-items-center">
+		  <img src="<?php if($wst_mid_slider6 !== $default){echo $wst_mid_slider6;} ?>" class="d-block w-100" alt="">
+		  <div class="carousel-caption col-md-4">
+			<h3><?php if($midslider6title !== $default){echo $midslider6title;} ?></h3>
+			<p><?php if($midsldr6text !== $default){echo $midsldr6text;} ?></p>
+		  </div>
+		</div>
+		<?php endif; ?>
 	  </div>
 	  <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
 	    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
