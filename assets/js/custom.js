a968357b94a3664010a7f974e61550a4592eec37
steelbridge-io <chris@steelbridge.io>
2020-09-21 17:28:36 -0700

    Updated footer nav scss and layout.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -15,21 +15,18 @@ $(document).on('click', '[data-toggle="lightbox"]', function(event) {
   $(this).ekkoLightbox();
 });
 
-// Shrink nav on scroll
+// Shrink nav on scroll and animate logo
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
 	 document.getElementById("genesis-nav-primary").style.padding = "0.3rem 1rem";
-	 //document.getElementById("nav-brand-link").style.top = "15px"
 	 document.getElementById("nav-brand-link").style.position = "absolute";
 	 document.getElementById("logo-svg-bmba").style.width = "142px";
 	 document.getElementById("logo-svg-bmba").style.height = "116px";
 	 document.getElementById("logo").style.width = "100px";
 	 document.getElementById("logo").style.height = "100px";
 	 document.getElementById("nav-brand-link").style.borderRadius = "0 0 50% 50%";
-	 //document.getElementById("Layer_1").style.padding = "10px 0";
-
 
 	 // Get the Object by ID
 	 var a = document.getElementById("logo");
@@ -39,8 +36,6 @@ function scrollFunction() {
 	 var svgItem = svgDoc.getElementById("Layer_1");
 	 // Set the colour to something else
 	 svgItem.style.padding = "5px 0";
-
-
 
   } else if (document.body.scrollTop < 80 || document.documentElement.scrollTop < 80) {
 	 document.getElementById("genesis-nav-primary").style.padding = ".5rem 1rem";
@@ -62,3 +57,4 @@ function scrollFunction() {
 	 svgItem.style.padding = "0";
   }
 }
+
