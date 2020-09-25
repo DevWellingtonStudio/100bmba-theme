ad9984aef2cc187a59f237762c0d5cc8758a7a8d
steelbridge-io <chris@steelbridge.io>
2020-09-24 18:26:47 -0700

    Fixed scroll issue where logo pre

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -77,7 +77,34 @@ $(document).on('click', '[data-toggle="lightbox"]', function(event) {
 				// Set the colour to something else
 				svgItem.style.padding = "0";
 			 }
+		  } else {
+		    '';
 		  }
 		}
+
+$(window).resize(function() {
+
+  if ($(window).width() < 1200 ) {
+
+	 document.getElementById("genesis-nav-primary").style.padding = ".5rem 1rem";
+	 document.getElementById("nav-brand-link").style.top = "0px"
+	 document.getElementById("nav-brand-link").style.position = "relative";
+	 document.getElementById("logo").style.width = "70px";
+	 document.getElementById("logo").style.height = "auto";
+	 document.getElementById("logo-svg-bmba").style.width = "70px";
+	 document.getElementById("logo-svg-bmba").style.height = "auto";
+	 document.getElementById("nav-brand-link").style.borderRadius = "0";
+
+	 // Get the Object by ID
+	 var a = document.getElementById("logo");
+	 // Get the SVG document inside the Object tag
+	 var svgDoc = a.contentDocument;
+	 // Get one of the SVG items by ID;
+	 var svgItem = svgDoc.getElementById("Layer_1");
+	 // Set the colour to something else
+	 svgItem.style.padding = "0";
+  }
+
+});
 
 
